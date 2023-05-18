524a8241ebf66f4a319c08ca16fe13642af279db
JapinhaMD <105886490+JapinhaMD@users.noreply.github.com>
2023-05-18 16:17:50 -0300

    Add files via upload

---
+++ TesteGamificação/GAmificacaoTeste/Gamificação_01 - Final/Gamificação_01 - Final/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using JRJ.Modas;
+
+namespace JRJ.Modas
+{
+    public class Program
+    {
+        public static List<ProdutoModel> produtos = new List<ProdutoModel>();
+        public static List<CategoriaModel> categorias = new List<CategoriaModel>();
+        
+        static void Main(string[] args)
+        {
+            bool continuar = true;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1 - Gerenciar produtos");
+                Console.WriteLine("2 - Gerenciar categorias");
+                Console.WriteLine("3 - Gerenciar clientes");
+                Console.WriteLine("4 - Realizar venda");
+                Console.WriteLine("5 - Mostrar relatório de vendas");
+                Console.WriteLine("0 - Sair");
+                Console.Write("Escolha uma opção: ");
+                string opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1":
+                        new ProdutoUI().GerenciarProdutos();
+                        break;
+                    case "2":
+                        new CategoriaUI().MenuDeFuncionalidades();
+
+                        //var ui = new CategoriaUI(categorias);
+                        //ui.MenuDeFuncionalidades();
+                        break;
+                    case "3":
+                        //GerenciarClientes();
+                        break;
+                    case "4":
+                        //RealizarVenda();
+                        break;
+                    case "5":
+                        //MostrarRelatorioVendas();
+                        break;
+                    case "0":
+                        continuar = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+                Console.WriteLine("Pressione qualquer tecla para continuar...");
+                Console.ReadKey();
+            } while (continuar);
+        }
+
+
+
+
+    }
+}
